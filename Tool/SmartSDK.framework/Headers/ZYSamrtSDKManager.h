46cac087b73e1de3da04cc979d453c5fd59b391f
MoMoLiangyu <2654574382@qq.com>
2022-07-13 21:04:29 +0800

    version 1.0.1

--- Tool/SmartSDK.framework/Headers/ZYSamrtSDKManager.h
+++ Tool/SmartSDK.framework/Headers/ZYSamrtSDKManager.h
@@ -11,11 +11,19 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ZYSamrtSDKManager : NSObject
 
+//单例
++(instancetype)share;
 
+//建立通信通道
+- (void)creatChannel;
+
+//第三方初始化
 + (void)initThirdManager;
 
+//直接跳转
 +(void)presnetSmarHome:(NSDictionary *)parameters;
 
+//通过控制器跳转
 +(void)parentVC:(id)parentVC toSmarHome:(NSDictionary *)parameters;
 
 @end
